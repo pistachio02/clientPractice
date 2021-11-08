8444db44ed1bb8e22a8c9d0e7ec64c2b78e34f4b
pistachio02 <wiaptm0219@gmail.com>
2021-11-08 18:41:02 +0900

    cliPrac

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { ModalBackdrop } from '../style/styled'
 import './Login.css'
 import LoginModal from './LoginModal'
-import { useHistory } from 'react-router-dom'
 
 function Login({ handleResponseSuccess }) {
 
