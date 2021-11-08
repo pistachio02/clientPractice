56740afccb3ce0891423d1b44c90bb2a9634bad7
Lee geon woo <78680884+Geonwoo-Lee@users.noreply.github.com>
2021-11-08 17:15:55 +0900

    건우

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ function App() {
           .then((res)=>{
             setIsLogin(true);
             setUserinfo(res.data.data.userInfo);
-            // history.push('/');
           })
     } else {
       axios
